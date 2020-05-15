1be81425eefd7a5de0a3eda0f0070e2dfc043812
Amanda Nascimento <amanda.nascimento@yaman.com.br>
2020-05-15 03:17:42 -0300

    iniciando automaçao

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -15,9 +15,13 @@ public class HomePage {
 	private Scenario cenario;
 	private DriverQAM driver;
 	
+	//banner
+	private String xpathLinkFecharBanner = "//*[@class=\"closeModal gtm-link-event\"]";
+	
 	// Localizacao
-	private String classSpanLocalizacaoCidade = "storefinderLocationComponent";
-	private String classSpanLocalizacaoEstado = "storefinderLocationComponentState";
+	private String xpathLinkLocalizacao = "//div[@class=\"yCmsContentSlot nav-locationCont\"]/div/a";
+	private String xpathSpanLocalizacaoCidade = "//div[@class=\"yCmsContentSlot nav-locationCont\"]/div/a/span[@class=\"storefinderLocationComponent\"]";
+	private String xpathSpanLocalizacaoEstado = "//div[@class=\"yCmsContentSlot nav-locationCont\"]/div/a/span[@class=\"storefinderLocationComponentState\"]";
 	private String idCboEstado = "address.region";
 	private String idTxtCidade = "addressTownCity";
 	private String idBtnEntrarLocal = "btnsendcookie";
@@ -40,20 +44,26 @@ public class HomePage {
 	
 	public void preencheLocalizacao(String estado, String cidade) {
 		
-		driver.click(classSpanLocalizacaoEstado, "class");
-		driver.waitElementAll(idCboEstado, "id");
-//		driver.selectByText(estado.toUpperCase(), idCboEstado, "id");
-//		driver.clear(idTxtCidade, "id");
-		driver.sendKeys(cidade.toUpperCase(), idTxtCidade, "id");
+		if (driver.waitElementTimeOut(xpathLinkFecharBanner, "xpath", 30)) {
+			driver.click(xpathLinkFecharBanner, "xpath");
+		}
+		
+		driver.click(xpathLinkLocalizacao, "xpath");
+		driver.waitElement(idCboEstado, "id");
+		driver.selectByText(estado.toUpperCase(), idCboEstado, "id");
+		driver.clear(idTxtCidade, "id");
+		driver.waitSeconds(2);
+//		driver.sendKeys(cidade.toUpperCase(), idTxtCidade, "id");
+		driver.sendKeysTab(cidade.toUpperCase(), idTxtCidade, "id");
 		driver.click(idBtnEntrarLocal, "id");
 		
 		driver.waitSeconds(3);
 		
-		if(driver.waitElementTimeOut(idCboEstado, "id", 3)) {
+		if(driver.waitElementTimeOut(xpathLinkLocalizacao, "id", 3)) {
 			driver.report(cenario, false, "Falha ao tentar selecionar a localização", true);
 		} else {
-			String cidadeAtual = driver.getText(classSpanLocalizacaoCidade, "class").toUpperCase();
-			String estadoAtual = driver.getText(classSpanLocalizacaoEstado, "class").toUpperCase();
+			String cidadeAtual = driver.getText(xpathSpanLocalizacaoCidade, "xpath").toUpperCase();
+			String estadoAtual = driver.getText(xpathSpanLocalizacaoEstado, "xpath").toUpperCase();
 			
 			if (!cidade.toUpperCase().contentEquals(cidadeAtual)) {
 				driver.report(cenario, false, "A cidade não foi alterada para " + cidade.toUpperCase(), true);
@@ -67,77 +77,77 @@ public class HomePage {
 		}
 		
 	}
-	
-	public void selecionaPlano(String nome, String tipo, String preco) {
-		
-		Globais.setNomePlano(nome);
-		Globais.setPrecoPlano(preco);
-		List<WebElement> planos = null;
-		WebElement paginacao = null;
-		
-		boolean achouPlano = false;
-		tipo = tipo.toUpperCase();
-
-		if (tipo.equals("CONTROLE")) {
-//			driver.moveToElement(classDivContainerPlanosControle, "class");
-//			planos = driver.findListElements(classDivContainerPlanosControle, "class");
-			
-		} else if (tipo.equals("PÓS-PAGO") || tipo.equals("POS-PAGO")) {
-//			driver.moveToElement(classDivContainerPlanosPos, "class");
-//			planos = driver.findListElements(classDivContainerPlanosPos, "class");
-		}
-		
-//		driver.browserScroll("down", 50);
-		paginacao = planos.get(0).findElement(By.className(classDivContainerPaginacao));
-		List<WebElement> cardsPlano = planos.get(0).findElements(By.className(classDivCardPlano));
-		
-		int pagina = 0;
-		
-		for (WebElement cardPlano : cardsPlano) {	
-			
-			if(cardPlano.getText().equals("")) {
-//			if(cont % 2 == 0) {
-				pagina++;
-				paginacao.findElements(By.className(classSpanPaginador)).get(pagina).click();
-				cardsPlano = planos.get(0).findElements(By.className(classDivCardPlano));
-				driver.waitSeconds(1);
-			}
-			
-			String tituloPlano = cardPlano.findElement(By.className("titulo-produto")).getText().toUpperCase();
-			
-			if (tituloPlano.equals(nome.toUpperCase())) {
-				//List<WebElement> botoes = cardPlano.findElements(By.tagName("button"));
-				List<WebElement> links = cardPlano.findElements(By.tagName("a"));
-				
-				for (WebElement link : links) {
-					if(link.getText().toUpperCase().equals(labelLink.toUpperCase())) {
-						
-						try {
-							link.click();
-						} catch (Exception e) { // Caso a imagem do whattsapp fique em cima do link Mais detalhes
-							driver.waitSeconds(1);
-							pagina++;
-							paginacao.findElements(By.className(classSpanPaginador)).get(pagina).click();
-							driver.waitSeconds(1);
-							link.click();
-						}
-						
-						achouPlano = true;
-						break;
-					}
-				}
-			} 
-			
-			if(achouPlano) break;
-		}
-		
-		if (achouPlano) {
-//			driver.waitElement(xpathBtnEuQuero, "xpath");
-			driver.report(cenario, true, "Mais detalhes do plano " + tipo + " - " + nome + " selecionado com sucesso" , true);
-
-		} else {
-			driver.report(cenario, false, "Mais detalhes do plano " + tipo + " - " + nome + " não foi encontrado", true);
-		}
-	
-	}
+//	
+//	public void selecionaPlano(String nome, String tipo, String preco) {
+//		
+//		Globais.setNomePlano(nome);
+//		Globais.setPrecoPlano(preco);
+//		List<WebElement> planos = null;
+//		WebElement paginacao = null;
+//		
+//		boolean achouPlano = false;
+//		tipo = tipo.toUpperCase();
+//
+//		if (tipo.equals("CONTROLE")) {
+////			driver.moveToElement(classDivContainerPlanosControle, "class");
+////			planos = driver.findListElements(classDivContainerPlanosControle, "class");
+//			
+//		} else if (tipo.equals("PÓS-PAGO") || tipo.equals("POS-PAGO")) {
+////			driver.moveToElement(classDivContainerPlanosPos, "class");
+////			planos = driver.findListElements(classDivContainerPlanosPos, "class");
+//		}
+//		
+////		driver.browserScroll("down", 50);
+//		paginacao = planos.get(0).findElement(By.className(classDivContainerPaginacao));
+//		List<WebElement> cardsPlano = planos.get(0).findElements(By.className(classDivCardPlano));
+//		
+//		int pagina = 0;
+//		
+//		for (WebElement cardPlano : cardsPlano) {	
+//			
+//			if(cardPlano.getText().equals("")) {
+////			if(cont % 2 == 0) {
+//				pagina++;
+//				paginacao.findElements(By.className(classSpanPaginador)).get(pagina).click();
+//				cardsPlano = planos.get(0).findElements(By.className(classDivCardPlano));
+//				driver.waitSeconds(1);
+//			}
+//			
+//			String tituloPlano = cardPlano.findElement(By.className("titulo-produto")).getText().toUpperCase();
+//			
+//			if (tituloPlano.equals(nome.toUpperCase())) {
+//				//List<WebElement> botoes = cardPlano.findElements(By.tagName("button"));
+//				List<WebElement> links = cardPlano.findElements(By.tagName("a"));
+//				
+//				for (WebElement link : links) {
+//					if(link.getText().toUpperCase().equals(labelLink.toUpperCase())) {
+//						
+//						try {
+//							link.click();
+//						} catch (Exception e) { // Caso a imagem do whattsapp fique em cima do link Mais detalhes
+//							driver.waitSeconds(1);
+//							pagina++;
+//							paginacao.findElements(By.className(classSpanPaginador)).get(pagina).click();
+//							driver.waitSeconds(1);
+//							link.click();
+//						}
+//						
+//						achouPlano = true;
+//						break;
+//					}
+//				}
+//			} 
+//			
+//			if(achouPlano) break;
+//		}
+//		
+//		if (achouPlano) {
+////			driver.waitElement(xpathBtnEuQuero, "xpath");
+//			driver.report(cenario, true, "Mais detalhes do plano " + tipo + " - " + nome + " selecionado com sucesso" , true);
+//
+//		} else {
+//			driver.report(cenario, false, "Mais detalhes do plano " + tipo + " - " + nome + " não foi encontrado", true);
+//		}
+//	
 }
+
--- src/test/java/support/DriverQAM.java
+++ src/test/java/support/DriverQAM.java
@@ -7,10 +7,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.collect.ImmutableMap;
 
@@ -25,25 +29,25 @@ public class DriverQAM {
 	private static AppiumDriver<MobileElement> driver;
 	@SuppressWarnings("unused")
 	private static Scenario cenario;
-	
+
 	public DriverQAM(Scenario cenario) {
 		DriverQAM.cenario = cenario;
 	}
-	
+
 	public void start(String OS) {
-		
+
 		String URL_STRING = "http://127.0.0.1:4723/wd/hub";
-		
+
 		URL url = null;
-		
+
 		try {
 			url = new URL(URL_STRING);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		
+
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		
+
 		switch (OS) {
 		case "ChromeMobile":
 			desiredCapabilities.setCapability("platformName", "Android");
@@ -51,85 +55,127 @@ public class DriverQAM {
 			desiredCapabilities.setCapability("noReset", true);
 			desiredCapabilities.setCapability("browserName", "Chrome");
 			desiredCapabilities.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
-			
+
 			driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			break;
 		}
 
 	}
-	
-    public void openURL(String parUrl) {
-        driver.get(parUrl);
-    }
-    
-    private String getAttributeType(String... parType) {
-        String type;
-        if (parType.length == 0) {
-            type = "id";
-        } else {
-            type = parType[0];
-        }
-        return type;
-    }
-    
-    public WebElement findElem(String parValue, String... parType) {
-        String param2 = getAttributeType(parType);
-        WebElement element = null;
 
-        try {
-            switch (param2) {
-                case "id":
-                    element = driver.findElement(MobileBy.id(parValue));
-                    break;
-                case "name":
-                    element = driver.findElement(MobileBy.name(parValue));
-                    break;
-                case "css":
-                    element = driver.findElement(MobileBy.cssSelector(parValue));
-                    break;
-                case "xpath":
-                    element = driver.findElement(MobileBy.xpath(parValue));
-                    break;
-                case "link":
-                    element = driver.findElement(MobileBy.linkText(parValue));
-                    break;
-                case "partialLink":
-                    element = driver.findElement(MobileBy.partialLinkText(parValue));
-                case "class":
-                    element = driver.findElement(MobileBy.className(parValue));
-                case "accessid":
-                    element = driver.findElementByAccessibilityId(parValue);
-                    break;
-                case "resourceid":
-                    element = driver.findElement(By.xpath("//*[@resource-id='" + parValue + "']"));
-                    break;
-                case "cd": // Content-desc
-                    element = driver.findElement(By.xpath("//*[@content-desc='" + parValue + "']"));
-                    break;
-                case "text": // Content-descwaitElementContainsTobeClickable
-                    element = driver.findElement(By.xpath("//*[@text='" + parValue + "']"));
-                    break;
-                case "contains":
-                    //element = driver.findElement(By.xpath("//*[contains(@content-desc,'" + parValue + "') or @text='" + parValue + "']"));
-                    element = driver.findElement(By.xpath("//*[contains(@content-desc,'" + parValue + "') or contains(@text,'" + parValue + "')]"));
-                    break;
-            }
-        } catch (NoSuchElementException e) {
-            element = null;
-        }
-        return element;
+	public void openURL(String parUrl) {
+		driver.get(parUrl);
+	}
+
+	private String getAttributeType(String... parType) {
+		String type;
+		if (parType.length == 0) {
+			type = "id";
+		} else {
+			type = parType[0];
+		}
+		return type;
+	}
+
+	public WebElement findElem(String parValue, String... parType) {
+		String param2 = getAttributeType(parType);
+		WebElement element = null;
+
+		try {
+			switch (param2) {
+			case "id":
+				element = driver.findElement(MobileBy.id(parValue));
+				break;
+			case "name":
+				element = driver.findElement(MobileBy.name(parValue));
+				break;
+			case "css":
+				element = driver.findElement(MobileBy.cssSelector(parValue));
+				break;
+			case "xpath":
+				element = driver.findElement(MobileBy.xpath(parValue));
+				break;
+			case "link":
+				element = driver.findElement(MobileBy.linkText(parValue));
+				break;
+			case "partialLink":
+				element = driver.findElement(MobileBy.partialLinkText(parValue));
+			case "class":
+				element = driver.findElement(MobileBy.className(parValue));
+			case "accessid":
+				element = driver.findElementByAccessibilityId(parValue);
+				break;
+			case "resourceid":
+				element = driver.findElement(By.xpath("//*[@resource-id='" + parValue + "']"));
+				break;
+			case "cd": // Content-desc
+				element = driver.findElement(By.xpath("//*[@content-desc='" + parValue + "']"));
+				break;
+			case "text": // Content-descwaitElementContainsTobeClickable
+				element = driver.findElement(By.xpath("//*[@text='" + parValue + "']"));
+				break;
+			case "contains":
+				// element = driver.findElement(By.xpath("//*[contains(@content-desc,'" +
+				// parValue + "') or @text='" + parValue + "']"));
+				element = driver.findElement(By.xpath(
+						"//*[contains(@content-desc,'" + parValue + "') or contains(@text,'" + parValue + "')]"));
+				break;
+			}
+		} catch (NoSuchElementException e) {
+			element = null;
+		}
+		return element;
+	}
+	
+	public void clear(String parValue, String... parType) {
+		try {
+			WebElement element = findElem(parValue, parType);
+			element.clear();
+		} catch (Exception e) {
+			report(cenario, false, "Não foi possível limpar campo " + parValue, true);
+		}
+	}
+	
+	public String getText(String parValue, String... parType) {
+
+		try {
+			WebElement element = findElem(parValue, parType);
+			return element.getText();
+		} catch (Exception e) {
+			report(cenario, false, "Não foi possível o texto do elemento " + parValue, true);
+			return null;
+		}
+	}
+
+    public void sendEnter() {
+        driver.getKeyboard().sendKeys(Keys.ENTER);
+    }
+	
+    public void sendKeysTab(String texto, String parValue, String... parType) {
+        WebElement element = findElem(parValue, parType);
+        element.sendKeys(texto, Keys.TAB);
     }
     
-    public void sendKeys(String parText, String parName, String... parType) {
-        WebElement element = findElem(parName, parType);
-        element.sendKeys(parText);
-    }
-    
-    public void waitSeconds(int time) {
-        driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
-    }
-    
+	public void sendKeys(String parText, String parName, String... parType) {
+		WebElement element = findElem(parName, parType);
+		element.sendKeys(parText);
+	}
+	
+	public void selectByText(String parText, String parName, String... parType) {
+		try {
+			WebElement element = findElem(parName, parType);
+			Select dropdown = new Select(element);
+			dropdown.selectByVisibleText(parText);
+		} catch (Exception e) {
+			report(cenario, false, "Não foi possível selecionar item " + parText + " no combobox " + parName,
+					true);
+		}
+	}
+
+	public void waitSeconds(int time) {
+		driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+	}
+
 	public void report(Scenario cenario, boolean status, String msg, boolean printScreen) {
 
 		cenario.write(msg);
@@ -143,12 +189,90 @@ public class DriverQAM {
 		}
 
 	}
-    
-    public void stop() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
-    }
-	
+
+	public void waitElement(String parName, String parType) {
+
+		boolean isVisible = true;
+
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 60);
+
+			switch (parType) {
+			case "id":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(parName))) == null) {
+					isVisible = false;
+				}
+				break;
+
+			case "class":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(parName))) == null) {
+					isVisible = false;
+				}
+				break;
+			}
+
+			if (!isVisible) {
+				report(cenario, false, "Falha ao aguardar o elemento " + parName, true);
+			}
+
+		} catch (Exception e) {
+			report(cenario, false, "Erro ao aguardar o elemento " + parName, true);
+		}
+
+	}
+
+	public void stop() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+	}
+
+	public void click(String parValue, String... parType) {
+		WebElement element = findElem(parValue, parType);
+		element.click();
+	}
+
+	public boolean waitElementTimeOut(String parName, String parType, int timeOut) {
+
+		boolean retorno = false;
+
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, timeOut);
+
+			switch (parType) {
+			case "id":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(parName))) != null) {
+					retorno = true;
+				}
+				break;
+
+			case "class":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(parName))) != null) {
+					retorno = true;
+				}
+				break;
+
+			case "xpath":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(parName))) != null) {
+					retorno = true;
+				}
+				break;
+
+			case "css":
+				if (wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(parName))) != null) {
+					retorno = true;
+				}
+				break;
+			}
+
+			return retorno;
+
+		} catch (Exception e) {
+//			report(cenario, false, "Erro ao aguardar o elemento " + parName, true);
+			return retorno;
+		}
+
+	}
+
 }
